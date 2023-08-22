aeec88c401c9cb109f9b8c57110b3b94709ef419
Filip <filip.oroz9@gmail.com>
2023-08-22 23:02:49 +0200

    minor fixes

--- src/App.js
+++ src/App.js
@@ -28,6 +28,7 @@ function App() {
       </nav>
       <Routes>
         <Route path="/" element={<Home isAuth={isAuth} />} />
+        <Route path="/home" element={<Home isAuth={isAuth} />} />
         <Route path="/login" element={<Login setAuth={setAuth} />} />
         <Route path="/createpost" element={<CreatePost isAuth={isAuth} />} />
       </Routes>
