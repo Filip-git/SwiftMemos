37aaab5179ae2e0c08660151a141c34627744fe0
Filip <filip.oroz9@gmail.com>
2023-08-31 02:37:17 +0200

    login page change

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,14 +1,19 @@
 import React, { useState } from "react";
 import { signInWithPopup, signInWithEmailAndPassword, createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
-import { MDBContainer, MDBTabs, MDBTabsItem, MDBTabsLink, MDBTabsContent, MDBTabsPane, MDBInput, MDBBtn } from "mdb-react-ui-kit";
+import { MDBContainer, MDBInput, MDBBtn, MDBRow, MDBCol, MDBCard, MDBCardBody, MDBIcon } from "mdb-react-ui-kit";
 import { auth, provider } from "../firebase-config";
 
 export function Login({ setAuth }) {
     let navigate = useNavigate();
-    const [justifyActive, setJustifyActive] = useState('tab1');
     const [loginError, setLoginError] = useState(null);
     const [registrationError, setRegistrationError] = useState(null);
+    const [isSignIn, setIsSignIn] = useState(true);
+
+
+    const toggleForm = () => {
+        setIsSignIn(!isSignIn);
+    };
 
     const signInWithGoogle = () => {
         signInWithPopup(auth, provider).then((result) => {
@@ -18,12 +23,6 @@ export function Login({ setAuth }) {
         });
     };
 
-    const handleJustifyClick = (value) => {
-        if (value === justifyActive) {
-            return;
-        }
-        setJustifyActive(value);
-    };
 
     const handleEmailLogin = async (event) => {
         event.preventDefault();
@@ -52,65 +51,57 @@ export function Login({ setAuth }) {
     };
 
     return (
-        <div className="loginPage">
-            <MDBContainer className="p-3 my-5 d-flex flex-column w-50">
-                <MDBTabs pills justify className='mb-3 d-flex flex-row justify-content-between'>
-                    <MDBTabsItem>
-                        <MDBTabsLink onClick={() => handleJustifyClick('tab1')} active={justifyActive === 'tab1'}>
-                            Login
-                        </MDBTabsLink>
-                    </MDBTabsItem>
-                    <MDBTabsItem>
-                        <MDBTabsLink onClick={() => handleJustifyClick('tab2')} active={justifyActive === 'tab2'}>
-                            Register
-                        </MDBTabsLink>
-                    </MDBTabsItem>
-                </MDBTabs>
+        <MDBContainer fluid>
+            <MDBRow className='d-flex justify-content-center align-items-center h-100'>
+                <MDBCol col='12'>
+                    <MDBCard className='bg-dark text-white my-5 mx-auto' style={{ borderRadius: '1.5rem', maxWidth: '800px' }}>
+                        <MDBCardBody className='p-5 d-flex flex-column align-items-center mx-auto w-100'>
+                            <h2 className="fw-bold mb-2 text-uppercase">{isSignIn ? 'Sign In' : 'Sign Up'}</h2>
+                            <p className="text-white-50 mb-5">{isSignIn ? 'Please enter your login and password' : 'Enter your desired username, email and password'}</p>
+                            {isSignIn ? (
+                                <form onSubmit={handleEmailLogin} className='w-100'>
+                                    <MDBInput wrapperClass='mb-4  w-100' labelClass='text-white' label='Email address' id='email' type='email' size="lg" />
+                                    <MDBInput wrapperClass='mb-4  w-100' labelClass='text-white' label='Password' id='password' type='password' size="lg" />
+                                    <div className="text-center"> <MDBBtn outline className='px-5' color='white' size='lg' type="submit">
+                                        Sign In
+                                    </MDBBtn> </div>
+                                    <div className='text-center mt-2 mb-3'>
+                                        <MDBBtn tag='a' color='none' className='m-3' style={{ color: 'white' }} onClick={signInWithGoogle}>
+                                            <MDBIcon fab icon='google' size="lg" />
+                                        </MDBBtn>
+                                    </div>
+                                    <p className="text-center mt-3">
+                                        Don't have an account? <a href="#!" className="text-white-50 fw-bold" onClick={toggleForm}>Sign Up</a>
+                                    </p>
+                                    {loginError && <p className="text-danger text-center mt-3">{loginError}</p>}
+                                </form>
+                            ) : (
+                                <form onSubmit={handleEmailRegister} className='w-100'>
+                                    <MDBInput wrapperClass='mb-4 w-100' labelClass='text-white' label='Username' id='username' name='regUsername' type='text' size="lg" />
+                                    <MDBInput wrapperClass='mb-4 w-100' labelClass='text-white' label='Email address' id='email' name='regEmail' type='email' size="lg" />
+                                    <MDBInput wrapperClass='mb-4 w-100' labelClass='text-white' label='Password' id='password' name='regPassword' type='password' size="lg" />
+                                    <div className="text-center">
+                                        <MDBBtn outline className='px-5' color='white' size='lg' type="submit">
+                                            Sign Up
+                                        </MDBBtn>
+                                    </div>
+                                    <div className='text-center mt-2 mb-3'>
+                                        <MDBBtn tag='a' color='none' className='m-3' style={{ color: 'white' }} onClick={signInWithGoogle}>
+                                            <MDBIcon fab icon='google' size="lg" />
+                                        </MDBBtn>
+                                    </div>
+                                    <p className="text-center mt-3">
+                                        Already have an account? <a href="#!" className="text-white-50 fw-bold" onClick={toggleForm}>Sign In</a>
+                                    </p>
+                                    {registrationError && <p className="text-danger text-center mt-3">{registrationError}</p>}
+                                </form>
 
-                <MDBTabsContent>
-
-                    <MDBTabsPane show={justifyActive === 'tab1'}>
-                        <div className="text-center mb-3">
-                            <button className="login-with-google-btn" onClick={signInWithGoogle}>
-                                Sign in with Google
-                            </button>
-                            <p className="text-center mt-3">or:</p>
-                        </div>
-
-                        {/* Email Login Form */}
-                        <form onSubmit={handleEmailLogin}>
-                            <MDBInput wrapperClass='mb-4' label='Email address' id='email' htmlFor='email' type='email' />
-                            <MDBInput wrapperClass='mb-4' label='Password' id='password' htmlFor='password' type='password' />
-                            <div className="text-center">
-                                <MDBBtn className="mb-4">Sign in</MDBBtn>
-                            </div>
-                            {loginError && <p className="error-message">{loginError}</p>}
-                        </form>
-
-                    </MDBTabsPane>
-
-                    <MDBTabsPane show={justifyActive === 'tab2'}>
-                        <div className="text-center mb-3">
-                            <button className="login-with-google-btn" onClick={signInWithGoogle}>
-                                Sign in with Google
-                            </button>
-                            <p className="text-center mt-3">or:</p>
-                        </div>
-
-                        {/* Email Registration Form */}
-                        <form onSubmit={handleEmailRegister}>
-                            <MDBInput wrapperClass='mb-4' label='Username' id='regUsername' type='text' />
-                            <MDBInput wrapperClass='mb-4' label='Email address' id='regEmail' htmlFor='email' type='email' />
-                            <MDBInput wrapperClass='mb-4' label='Password' id='regPassword' htmlFor='password' type='password' />
-                            <div className="text-center">
-                                <MDBBtn className="mb-4">Sign up</MDBBtn>
-                            </div>
-                            {registrationError && <p className="error-message">{registrationError}</p>}
-                        </form>
-                    </MDBTabsPane>
-                </MDBTabsContent>
-            </MDBContainer>
-        </div>
+                            )}
+                        </MDBCardBody>
+                    </MDBCard>
+                </MDBCol>
+            </MDBRow>
+        </MDBContainer>
     );
 }
 
